3f3b3e58e7ef3924498886a8d68fd56077499fc4
Uuviee <uuvieealtan@gmail.com>
2023-03-15 01:57:04 +0000

    commit

--- Class1/first_script.sh
+++ Class1/first_script.sh
@@ -3,4 +3,4 @@
 echo "This script name is $0" ##  $0 represensts script itself by presesnting the name 
 echo "Hello there $1 $2  $@" ##
 echo "This is the list of all aarguments given by the user: $@"
-ECho "This script is running with this PID: $$"
+echo "This script is running with this PID: $$"
